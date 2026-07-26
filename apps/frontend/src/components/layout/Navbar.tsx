8f3ead8ef72e48beae40b8ec135e7cee31ccf298
mcwachira <mcwachira@outlook.com>
2026-07-26 15:51:54 +0300

    setup use-cart for cart functionality and setup also the site layout

---
+++ apps/frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import {Link} from "@tanstack/react-router"
+import {Moon, Sun,ShoppingBag, Search, User,Menu } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
