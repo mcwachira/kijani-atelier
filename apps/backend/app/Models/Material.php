c9ae9dee93cac35dad479343dfa5b0fb0cd64026
mcwachira <mcwachira@outlook.com>
2026-08-06 12:27:41 +0300

    Add HasFactory trait to catalog and order models

--- apps/backend/app/Models/Material.php
+++ apps/backend/app/Models/Material.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Material extends Model
 {
+
+    use HasFactory;
     protected $fillable = ['name'];
 
     public function products(): BelongsToMany
--- apps/backend/app/Models/Order.php
+++ apps/backend/app/Models/Order.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
-
+    use HasFactory;
     protected $fillable = ['user_id','reference', 'customer_name', 'email','phone',
          'county', 'town', 'address', 'payment_method',];
 
